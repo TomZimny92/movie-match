5a03595602d0b490ea48ee5b9836f280dc168ac7
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2025-06-30 18:41:44 -0400

    some boilerplate

---
+++ movie-match/components/header/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <h1>Movie Match!</h1>
+            <h3>See what projects different artists worked on together</h3>
+        </div>
+        <div>
+            <UButton label="How Do" color="info"></UButton>
+            <UButton label="Give Me Money" color="primary"></UButton>
+        </div>
+    </div>
+
+</template>
